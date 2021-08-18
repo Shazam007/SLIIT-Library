39c941b44e4ad044e3b9fa3df4e2215233011306
Dinuka Amarasinghe <42789915+dinuka1998@users.noreply.github.com>
2021-08-18 17:23:21 +0530

    Book list added

--- Assets/Booklist.cs
+++ Assets/Booklist.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Booklist : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    [Serializable]
+    public struct Book{
+        public string Name;
+        public string Author;
+        public string Availability;
+        public Sprite Cover;
+
+    }
+
+    [SerializeField] Book[] allBooks;
+
     void Start()
     {
         UnlockMouse();
@@ -12,9 +27,15 @@ public class Booklist : MonoBehaviour
         GameObject buttonTemplate = transform.GetChild(0).gameObject;
         GameObject g;
 
-        for(int i = 0 ; i < 5 ; i++){
-            g = Instantiate (buttonTemplate, transform);
-        }
+        int N = allBooks.Length;
+
+        for(int i = 0 ; i < N ; i++){
+            g = Instantiate (buttonTemplate, this.transform);
+            g.transform.GetChild(0).gameObject.GetComponent <Text>().text = allBooks[i].Name;
+            g.transform.GetChild(1).gameObject.GetComponent <Text>().text = allBooks[i].Author;
+            g.transform.GetChild(2).gameObject.GetComponent <Text>().text = allBooks[i].Availability;
+            g.transform.GetChild(3).gameObject.GetComponent <Image>().sprite = allBooks[i].Cover;
+         }
 
         Destroy(buttonTemplate);
     }
